555e762712f15140e8219c14143a94a15bc7be68
alexiastandish <alexiastandish@gmail.com>
2019-12-11 07:47:39 -0800

    fixed form

--- components/contact-section/contact-form/index.js
+++ components/contact-section/contact-form/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Button, Body, Input } from "../../../styles/common";
+import React, { useState, useEffect } from "react";
+import { Button, Body, Input, Header } from "../../../styles/common";
 import { StyledForm } from "./styles";
 import Form from "./form";
 import formValidationSchema from "./validationSchema";
@@ -17,15 +17,25 @@ const ContactForm = () => {
   };
 
   const [fields, updateForm] = useFormState(initialState);
+  const [success, setSuccess] = useState("");
 
   const handleFormSubmit = () => {
-    axios
-      .post("https://formcarry.com/s/5YodmAHlQNY", fields, {
-        headers: { Accept: "application/json" }
-      })
-      .then(res => console.log("res", res))
-      .catch(error => console.log("error", error));
+    try {
+      axios
+        .post("https://formcarry.com/s/5YodmAHlQNY", fields, {
+          headers: { Accept: "application/json" }
+        })
+        .then(res => {
+          if (res.data.status === "success") {
+            return setSuccess(res.data.title);
+          }
+        });
+    } catch (error) {
+      error => console.log("error", error);
+    }
   };
+
+  // https://formcarry.com/s/KLcuW51aWDD
 
   return (
     <Form
@@ -36,62 +46,67 @@ const ContactForm = () => {
       {({ errors, submitFormWithValidation }) => {
         return (
           <StyledForm>
-            <InputWrapper
-              title="First Name"
-              error={getFormErrorText(errors, "fname")}
-            >
-              <Input
-                type="text"
-                name="fname"
-                value={fields.fname || ""}
-                error={errors && errors["fname"]}
-                onChange={updateForm}
-              />
-            </InputWrapper>
-            <InputWrapper
-              title="Last Name"
-              error={getFormErrorText(errors, "lname")}
-            >
-              <Input
-                type="text"
-                name="lname"
-                // placeholder="Last name"
-                error={errors && errors["lname"]}
-                value={fields.lname || ""}
-                onChange={updateForm}
-              />
-            </InputWrapper>
-            <InputWrapper
-              title="Email"
-              error={getFormErrorText(errors, "email")}
-            >
-              <Input
-                type="email"
-                name="email"
-                aria-describedby="emailHelp"
-                error={errors && errors["email"]}
-                // placeholder="Your Email Here"
-                value={fields.email || ""}
-                onChange={updateForm}
-              />
-            </InputWrapper>
-
-            <InputWrapper
-              title="Message"
-              error={getFormErrorText(errors, "message")}
-            >
-              <textarea
-                placeholder="Leave a message..."
-                error={errors && errors["message"]}
-                name="message"
-                rows="5"
-                value={fields.message || ""}
-                onChange={updateForm}
-              />
-            </InputWrapper>
-            <Button type="primary" onClick={submitFormWithValidation}>
-              Submit
-            </Button>
+            {!success ? (
+              <>
+                <InputWrapper
+                  title="First Name"
+                  error={getFormErrorText(errors, "fname")}
+                >
+                  <Input
+                    type="text"
+                    name="fname"
+                    value={fields.fname || ""}
+                    error={errors && errors["fname"]}
+                    onChange={updateForm}
+                  />
+                </InputWrapper>
+                <InputWrapper
+                  title="Last Name"
+                  error={getFormErrorText(errors, "lname")}
+                >
+                  <Input
+                    type="text"
+                    name="lname"
+                    // placeholder="Last name"
+                    error={errors && errors["lname"]}
+                    value={fields.lname || ""}
+                    onChange={updateForm}
+                  />
+                </InputWrapper>
+                <InputWrapper
+                  title="Email"
+                  error={getFormErrorText(errors, "email")}
+                >
+                  <Input
+                    type="email"
+                    name="email"
+                    aria-describedby="emailHelp"
+                    error={errors && errors["email"]}
+                    // placeholder="Your Email Here"
+                    value={fields.email || ""}
+                    onChange={updateForm}
+                  />
+                </InputWrapper>
+                <InputWrapper
+                  title="Message"
+                  error={getFormErrorText(errors, "message")}
+                >
+                  <textarea
+                    placeholder="Leave a message..."
+                    error={errors && errors["message"]}
+                    name="message"
+                    rows="5"
+                    value={fields.message || ""}
+                    onChange={updateForm}
+                  />
+                </InputWrapper>
+                <Button type="primary" onClick={submitFormWithValidation}>
+                  Submit
+                </Button>
+              </>
+            ) : (
+              <Header>{success}</Header>
+            )}
           </StyledForm>
         );
       }}
